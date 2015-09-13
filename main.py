43774eefb6c6d56ead91e33ca21ce8d17ad16400
Thor H. Jonsson <thor.h.jonsson@gmail.com>
2015-09-13 11:18:03 +0000

    imported pdb to main

--- main.py
+++ main.py
@@ -1,13 +1,14 @@
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
+import pdb
 
 if __name__ == "__main__":
 
 	filename = 'air_data.csv'
         dateparse = lambda x: pd.datetime.strptime(x, '%d.%m.%Y %H:%M')
 	air_data = pd.read_csv(filename, sep='\t', parse_dates=[['Date', 'Time']], date_parser = dateparse, index_col=['Date_Time'], comment='#')
-
+        pdb.set_trace()
         air_data['SO2'].plot()
 	plt.show()
 
